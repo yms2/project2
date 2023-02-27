f9dc402c4be263079361b6ddd2d66684363e0d46
Yu Min Seung <90673332+yms2@users.noreply.github.com>
2023-02-27 18:12:08 +0900

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace project2
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 필수 디자이너 변수입니다.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 사용 중인 모든 리소스를 정리합니다.
+		/// </summary>
+		/// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 디자이너에서 생성한 코드
+
+		/// <summary>
+		/// 디자이너 지원에 필요한 메서드입니다. 
+		/// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.uiGridView_CSV = new System.Windows.Forms.DataGridView();
+            this.uiBtn_ReadCsv = new System.Windows.Forms.Button();
+            this.uiBtn_ReadCsv2 = new System.Windows.Forms.Button();
+            this.btnCSV_Save = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.uiGridView_CSV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // uiGridView_CSV
+            // 
+            this.uiGridView_CSV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.uiGridView_CSV.Location = new System.Drawing.Point(193, 31);
+            this.uiGridView_CSV.Name = "uiGridView_CSV";
+            this.uiGridView_CSV.RowTemplate.Height = 23;
+            this.uiGridView_CSV.Size = new System.Drawing.Size(727, 383);
+            this.uiGridView_CSV.TabIndex = 0;
+            // 
+            // uiBtn_ReadCsv
+            // 
+            this.uiBtn_ReadCsv.Location = new System.Drawing.Point(47, 31);
+            this.uiBtn_ReadCsv.Name = "uiBtn_ReadCsv";
+            this.uiBtn_ReadCsv.Size = new System.Drawing.Size(127, 69);
+            this.uiBtn_ReadCsv.TabIndex = 1;
+            this.uiBtn_ReadCsv.Text = "Call";
+            this.uiBtn_ReadCsv.UseVisualStyleBackColor = true;
+            this.uiBtn_ReadCsv.Click += new System.EventHandler(this.uiBtn_ReadCsv_Click);
+            // 
+            // uiBtn_ReadCsv2
+            // 
+            this.uiBtn_ReadCsv2.Location = new System.Drawing.Point(47, 139);
+            this.uiBtn_ReadCsv2.Name = "uiBtn_ReadCsv2";
+            this.uiBtn_ReadCsv2.Size = new System.Drawing.Size(127, 69);
+            this.uiBtn_ReadCsv2.TabIndex = 3;
+            this.uiBtn_ReadCsv2.Text = "Call";
+            this.uiBtn_ReadCsv2.UseVisualStyleBackColor = true;
+            this.uiBtn_ReadCsv2.Click += new System.EventHandler(this.uiBtn_ReadCsv2_Click);
+            // 
+            // btnCSV_Save
+            // 
+            this.btnCSV_Save.Location = new System.Drawing.Point(47, 248);
+            this.btnCSV_Save.Name = "btnCSV_Save";
+            this.btnCSV_Save.Size = new System.Drawing.Size(127, 73);
+            this.btnCSV_Save.TabIndex = 4;
+            this.btnCSV_Save.Text = "Save";
+            this.btnCSV_Save.UseVisualStyleBackColor = true;
+            this.btnCSV_Save.Click += new System.EventHandler(this.btnCSV_Save_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(947, 450);
+            this.Controls.Add(this.btnCSV_Save);
+            this.Controls.Add(this.uiBtn_ReadCsv2);
+            this.Controls.Add(this.uiBtn_ReadCsv);
+            this.Controls.Add(this.uiGridView_CSV);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.uiGridView_CSV)).EndInit();
+            this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.DataGridView uiGridView_CSV;
+		private System.Windows.Forms.Button uiBtn_ReadCsv;
+		private System.Windows.Forms.Button uiBtn_ReadCsv2;
+        private System.Windows.Forms.Button btnCSV_Save;
+    }
+}
+
